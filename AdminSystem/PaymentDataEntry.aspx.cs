d0c85539f67d8433d3e4c4d4616ee4263d3221d9
p2681917 <p2681917@my365.dmu.ac.uk>
2023-04-27 20:29:14 +0100

    today branch

--- AdminSystem/PaymentDataEntry.aspx.cs
+++ AdminSystem/PaymentDataEntry.aspx.cs
@@ -23,14 +23,14 @@ public partial class _1_DataEntry : System.Web.UI.Page
 
     }
 
-    protected void Button1_Click(object sender, EventArgs e)
+    protected void btnPay_Click(object sender, EventArgs e)
     {
         //create a new instance of clsAddress
         clsPayment AnPayment = new clsPayment();
         //capture the name on card
         AnPayment.NameOnCard = txtNameonCard.Text;
         AnPayment.CardNumber = Convert.ToInt32(txtCardNumber.Text);
-        AnPayment.Cvv = Convert.ToInt32(txtCvv.Text);
+        AnPayment.cvv = Convert.ToInt32(txtCvv.Text);
         AnPayment.PostalCode = txtPostalCode.Text;
         AnPayment.Active = chkActive.Checked;
         AnPayment.ExparationDate = Convert.ToDateTime(txtExparationDate);
@@ -43,5 +43,31 @@ public partial class _1_DataEntry : System.Web.UI.Page
         Response.Redirect("PaymentViewer.aspx");
     }
 
-    
+
+
+    protected void btnFind_Click(object sender, EventArgs e)
+    {
+        clsPayment AnPayment = new clsPayment();
+        Int32 PymNo;
+        Boolean Found = false;
+        PymNo = Convert.ToInt32(txtPymNo.Text);
+        Found = AnPayment.Find(PymNo);
+        if (Found == true)
+        {
+            txtCardNumber.Text = AnPayment.CardNumber.ToString();
+            txtCvv.Text = AnPayment.cvv.ToString();
+            txtExparationDate.Text = AnPayment.ExparationDate.ToString();
+            txtNameonCard.Text = AnPayment.NameOnCard;
+            txtPostalCode.Text = AnPayment.PostalCode;
+            chkActive.Checked = AnPayment.Active;
+
+
+        }
+
+    }
+
+    protected void TextBox1_TextChanged2(object sender, EventArgs e)
+    {
+
+    }
 }
--- ClassLibrary/clsPayment.cs
+++ ClassLibrary/clsPayment.cs
@@ -4,15 +4,131 @@ namespace ClassLibrary
 {
     public class clsPayment
     {
-        public int cardNumber;
-        public int cvv;
-        public int customerName;
+        //Private data me
+        private Int32 mPymNo;
+        public int mCardNumber;
+        private int mcvv;
+        public string mNameOnCard;
+        private DateTime mExparationDate;
+        private bool mActive;
+        private string mPostalCode;
 
-        public bool Active { get; set; }
-        public DateTime ExparationDate { get; set; }
-        public string NameOnCard { get; set; }
-        public int CardNumber { get; set; }
-        public int Cvv { get; set; }
-        public string PostalCode { get; set; }
+     
+        public Int32 PymNo
+        {
+            get
+            {
+                return mPymNo;
+
+            }
+            set
+            {
+                mPymNo = value;
+            }
+        }
+        public DateTime ExparationDate
+        {
+            get
+            {
+                return mExparationDate;
+            }
+            set
+            {
+                mExparationDate = value;
+            }
+
+        }
+        public Int32 cvv
+        {
+            get
+            {
+                return mcvv;
+
+            }
+            set
+            {
+                mcvv = value;
+            }
+        }
+        public string NameOnCard
+        {
+            get
+            {
+                return mNameOnCard;
+
+            }
+            set
+            {
+                mNameOnCard = value;
+            }
+        }
+        public int CardNumber
+        {
+            get
+            {
+                return mCardNumber;
+
+            }
+            set
+            {
+                mCardNumber = value;
+            }
+        }
+        public bool Active
+        {
+            get
+            {
+                return mActive;
+
+            }
+            set
+            {
+                mActive = value;
+            }
+        }
+        public string PostalCode
+        {
+            get
+            {
+                return mPostalCode;
+
+            }
+            set
+            {
+                mPostalCode = value;
+            }
+        }
+
+
+
+
+        public bool Find(int PymNo)
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@PymNo", PymNo);
+            DB.Execute("sproc_tblPayment_FilterByPymNo");
+            if (DB.Count == 1)
+            {
+                mPymNo = Convert.ToInt32(DB.DataTable.Rows[0]["PymNo"]);
+                mNameOnCard = Convert.ToString(DB.DataTable.Rows[0]["NameOnCard"]);
+                mCardNumber = Convert.ToInt32(DB.DataTable.Rows[0]["cardNumber"]);
+                mExparationDate = Convert.ToDateTime(DB.DataTable.Rows[0]["ExparationDate"]);
+                mcvv = Convert.ToInt32(DB.DataTable.Rows[0]["cvv"]);
+                mPostalCode = Convert.ToString(DB.DataTable.Rows[0]["PostalCode"]);
+                mActive = Convert.ToBoolean(DB.DataTable.Rows[0]["Save"]);
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+          public string Valid(string exparationDate, string postalCode, string cardNumber, string cvv)
+        {
+            return "";
+        }
     }
-}
+    }
+
+       
