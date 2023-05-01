ffbe531a2ea257bc23dc7ee8e382039e0970174d
P2672211 <P2672211@my365.dmu.ac.uk>
2023-05-01 10:20:46 +0100

    Added test methods for find for all properties

--- ClassLibrary/ClsOrder.cs
+++ ClassLibrary/ClsOrder.cs
@@ -4,10 +4,86 @@ namespace ClassLibrary
 {
     public class clsOrder
     {
-        public DateTime DateOrdered { get; set; }
-        public string CustomerAddress { get; set; }
-        public string PaymentMethod { get; set; }
-        public decimal Amount { get; set; }
-        public Boolean Paid { get; set; }
+        // private data members for properties
+        private Int32 mOrderID;
+        private string mCustomerAddress;
+        private DateTime mDateOrdered;
+        private string mPaymentMethod;
+        private decimal mAmount;
+        private Boolean mPaid;
+       
+
+        public DateTime DateOrdered 
+        { 
+            get
+            {
+                return mDateOrdered;
+            }
+            set
+            {
+                mDateOrdered = value;
+            }
+        }
+        public string CustomerAddress { 
+            get
+            {
+                // this line of code sends data out of the property
+                return mCustomerAddress;
+            }
+            set
+            {
+                // this line of code allows data into the property
+                mCustomerAddress = value;
+            }
+        }
+        public string PaymentMethod 
+        {
+            get
+            {
+                return mPaymentMethod;
+            }
+            set
+            {
+                mPaymentMethod = value;
+            }
+        }
+        public decimal Amount
+        {
+            get
+            {
+                return mAmount;
+            }
+
+            set
+            {
+                mAmount = value;
+            }
+        }
+        public Boolean Paid
+        {
+            get
+            {
+                return mPaid;
+            }
+
+            set
+            {
+                mPaid = value;
+            }
+        }
+
+        public bool Find(int OrderID)
+        {
+            // set the private data members to the test data value
+            mOrderID = 1;
+            mCustomerAddress = "123 Main Street";
+            mDateOrdered = Convert.ToDateTime("16/9/2023");
+            mPaymentMethod = "Credit Card";
+            mAmount = 10.99m;
+            mPaid = true;
+
+            // always return true
+            return true;
+        }
     }
 }
